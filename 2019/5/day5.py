545206073398ed3033d1acb91dffe8d9dd4a5301
Svetlana Velichkova <svetlana.velichkova@ft.com>
2021-04-21 16:28:39 +0300

    day 5 part 2

--- 2019/5/day5.py
+++ 2019/5/day5.py
@@ -1,10 +1,17 @@
 input = open("2019/5/input.txt", "r")
 int_operations = list(map(int, input.read().split(",")))
 
+
+
+
 SUM = 1
 MULTIPLY = 2
 SAVE = 3
 PRINT = 4
+JUMP_IF_TRUE = 5
+JUMP_IF_FALSE = 6
+LESS_THAN = 7
+EQUALS = 8
 HALT = 99
 
 POSITION = "POSITION"
@@ -14,7 +21,11 @@ OPERATIONS = {
     SUM: 3,
     MULTIPLY: 3,
     SAVE: 1,
-    PRINT: 1
+    PRINT: 1,
+    JUMP_IF_TRUE: 2,
+    JUMP_IF_FALSE: 2,
+    LESS_THAN: 3,
+    EQUALS: 3
 }
 
 MODES = {
@@ -60,7 +71,7 @@ def multiplyOp(i, operations, modes):
     return 4
 
 def saveOp(i, operations, modes):
-    operations[operations[i + 1]] = 1 #TODO: learn how to take input
+    operations[operations[i + 1]] = 5 #TODO: learn how to take input
     return 2
 
 def printOp(i, operations, modes):
@@ -72,11 +83,63 @@ def printOp(i, operations, modes):
     print(parameter)
     return 2
 
+def jumpIfTrueOp(i, operations, modes):
+    return jumpOp(i, operations, modes, True)
+
+def jumpIfFalseOp(i, operations, modes):
+    return jumpOp(i, operations, modes, False)
+
+def jumpOp(i, operations, modes, isIfTrue):
+    if len(modes) >= 1:
+        first_mode = modes[-1]
+    else:
+        first_mode = '0'
+    if len(modes) >= 2:
+        second_mode = modes[-2]
+    else:
+        second_mode = '0'
+    parameter = get_parameter_value(operations[i + 1], first_mode, operations)
+    second_parameter = get_parameter_value(operations[i + 2], second_mode, operations)
+    if((parameter != 0 and isIfTrue) or
+    (parameter == 0 and not isIfTrue)):
+        return second_parameter - i
+    else:
+        return 3
+
+def lessThanOp(i, operations, modes):
+    return compareOp(i, operations, modes, lambda a, b: a < b)
+
+def equalsOp(i, operations, modes):
+    return compareOp(i, operations, modes, lambda a, b: a == b)
+
+def compareOp(i, operations, modes, comparator):
+    if len(modes) >= 1:
+        first_mode = modes[-1]
+    else:
+        first_mode = '0'
+    if len(modes) >= 2:
+        second_mode = modes[-2]
+    else:
+        second_mode = '0'
+    first_parameter = get_parameter_value(operations[i + 1], first_mode, operations)
+    second_parameter = get_parameter_value(operations[i + 2], second_mode, operations)
+    if(comparator(first_parameter, second_parameter)):
+        operations[operations[i+3]] = 1
+    else:
+        operations[operations[i+3]] = 0
+    return 4
+
+
+
 FUNCTIONS = {
     SUM: sumOp,
     MULTIPLY: multiplyOp,
     SAVE: saveOp,
-    PRINT: printOp
+    PRINT: printOp,
+    JUMP_IF_TRUE: jumpIfTrueOp,
+    JUMP_IF_FALSE: jumpIfFalseOp,
+    LESS_THAN: lessThanOp,
+    EQUALS: equalsOp
 }
 
 def execute_operation(i, operations, opcode, modes):
@@ -92,6 +155,7 @@ def run_int_code(operations):
             modes = str(operations[i])[:-2]
             opcode = int(str(operations[i])[-2:])
             i += execute_operation(i, operations, opcode, modes)
+    print(operations)
     return operations[0]
 
 print(run_int_code(int_operations))
