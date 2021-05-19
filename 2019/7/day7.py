466bd607c6c89d05824cc98244530339a6981ebf
Nick Ramsbottom <nick.ramsbottom@ft.com>
2021-05-19 14:11:18 +0100

    Create day 7 boilerplate with copied int computer from day 5

---
+++ 2019/7/day7.py
@@ -0,0 +1,158 @@
+input = open("2019/5/input.txt", "r")
+int_operations = list(map(int, input.read().split(",")))
+
+SUM = 1
+MULTIPLY = 2
+SAVE = 3
+PRINT = 4
+JUMP_IF_TRUE = 5
+JUMP_IF_FALSE = 6
+LESS_THAN = 7
+EQUALS = 8
+HALT = 99
+
+POSITION = "POSITION"
+IMMEDIATE = "IMMEDIATE"
+
+OPERATIONS = {
+    SUM: 3,
+    MULTIPLY: 3,
+    SAVE: 1,
+    PRINT: 1,
+    JUMP_IF_TRUE: 2,
+    JUMP_IF_FALSE: 2,
+    LESS_THAN: 3,
+    EQUALS: 3
+}
+
+MODES = {
+    POSITION: '0',
+    IMMEDIATE: '1'
+}
+
+def get_parameter_value(parameter, mode, operations):
+    if mode == MODES[POSITION]:
+        return int(operations[int(parameter)])
+    elif mode == MODES[IMMEDIATE]:
+        return int(parameter)
+    raise Exception(f'Unknown mode {mode}')
+
+def sumOp(i, operations, modes):
+    if len(modes) >= 1:
+        first_mode = modes[-1]
+    else:
+        first_mode = '0'
+    if len(modes) >= 2:
+        second_mode = modes[-2]
+    else:
+        second_mode = '0'
+    first_parameter = get_parameter_value(operations[i + 1], first_mode, operations)
+    second_parameter = get_parameter_value(operations[i + 2], second_mode, operations)
+    sum_index = operations[i + 3]
+    operations[sum_index] = first_parameter + second_parameter
+    return 4
+
+def multiplyOp(i, operations, modes):
+    if len(modes) >= 1:
+        first_mode = modes[-1]
+    else:
+        first_mode = '0'
+    if len(modes) >= 2:
+        second_mode = modes[-2]
+    else:
+        second_mode = '0'
+    first_parameter = get_parameter_value(operations[i + 1], first_mode, operations)
+    second_parameter = get_parameter_value(operations[i + 2], second_mode, operations) #TODO: leading zero problem
+    product_index = operations[i + 3]
+    operations[product_index] = first_parameter * second_parameter
+    return 4
+
+def saveOp(i, operations, modes):
+    operations[operations[i + 1]] = 5 #TODO: learn how to take input
+    return 2
+
+def printOp(i, operations, modes):
+    if len(modes) >= 1:
+        first_mode = modes[-1]
+    else:
+        first_mode = '0'
+    parameter = get_parameter_value(operations[i + 1], first_mode, operations)
+    print(parameter)
+    return 2
+
+def jumpIfTrueOp(i, operations, modes):
+    return jumpOp(i, operations, modes, True)
+
+def jumpIfFalseOp(i, operations, modes):
+    return jumpOp(i, operations, modes, False)
+
+def jumpOp(i, operations, modes, isIfTrue):
+    if len(modes) >= 1:
+        first_mode = modes[-1]
+    else:
+        first_mode = '0'
+    if len(modes) >= 2:
+        second_mode = modes[-2]
+    else:
+        second_mode = '0'
+    parameter = get_parameter_value(operations[i + 1], first_mode, operations)
+    second_parameter = get_parameter_value(operations[i + 2], second_mode, operations)
+    if((parameter != 0 and isIfTrue) or
+    (parameter == 0 and not isIfTrue)):
+        return second_parameter - i
+    else:
+        return 3
+
+def lessThanOp(i, operations, modes):
+    return compareOp(i, operations, modes, lambda a, b: a < b)
+
+def equalsOp(i, operations, modes):
+    return compareOp(i, operations, modes, lambda a, b: a == b)
+
+def compareOp(i, operations, modes, comparator):
+    if len(modes) >= 1:
+        first_mode = modes[-1]
+    else:
+        first_mode = '0'
+    if len(modes) >= 2:
+        second_mode = modes[-2]
+    else:
+        second_mode = '0'
+    first_parameter = get_parameter_value(operations[i + 1], first_mode, operations)
+    second_parameter = get_parameter_value(operations[i + 2], second_mode, operations)
+    if(comparator(first_parameter, second_parameter)):
+        operations[operations[i+3]] = 1
+    else:
+        operations[operations[i+3]] = 0
+    return 4
+
+
+
+FUNCTIONS = {
+    SUM: sumOp,
+    MULTIPLY: multiplyOp,
+    SAVE: saveOp,
+    PRINT: printOp,
+    JUMP_IF_TRUE: jumpIfTrueOp,
+    JUMP_IF_FALSE: jumpIfFalseOp,
+    LESS_THAN: lessThanOp,
+    EQUALS: equalsOp
+}
+
+def execute_operation(i, operations, opcode, modes):
+    function = FUNCTIONS[opcode]
+    return function(i, operations, modes)
+
+def run_int_code(operations):
+    i = 0
+    while i < len(operations):
+        if operations[i] == HALT:
+            break
+        else:
+            modes = str(operations[i])[:-2]
+            opcode = int(str(operations[i])[-2:])
+            i += execute_operation(i, operations, opcode, modes)
+    print(operations)
+    return operations[0]
+
+print(run_int_code(int_operations))
