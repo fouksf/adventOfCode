abc6c18be17b811ed8214da30e8c863e121ec4c5
Svetlana Velichkova <svetlana.velichkova@ft.com>
2021-08-11 16:05:42 +0300

    Try to take out amplifier into a separate class

--- 2019/7/amplifier.py
+++ 2019/7/amplifier.py
@@ -1,48 +1,44 @@
-SUM = 1
-MULTIPLY = 2
-SAVE = 3
-PRINT = 4
-JUMP_IF_TRUE = 5
-JUMP_IF_FALSE = 6
-LESS_THAN = 7
-EQUALS = 8
-HALT = 99
+from enum import Enum
 
-POSITION = "POSITION"
-IMMEDIATE = "IMMEDIATE"
+class Operation(Enum):
+    SUM = 1
+    MULTIPLY = 2
+    SAVE = 3
+    PRINT = 4
+    JUMP_IF_TRUE = 5
+    JUMP_IF_FALSE = 6
+    LESS_THAN = 7
+    EQUALS = 8
+    HALT = 99
 
-OPERATIONS = {
-    SUM: 3,
-    MULTIPLY: 3,
-    SAVE: 1,
-    PRINT: 1,
-    JUMP_IF_TRUE: 2,
-    JUMP_IF_FALSE: 2,
-    LESS_THAN: 3,
-    EQUALS: 3
-}
-
-MODES = {
-    POSITION: '0',
-    IMMEDIATE: '1'
-}
+class Mode(Enum):
+    POSITION = "0"
+    IMMEDIATE = "1"
 
 class Amplifier:
     def __init__(self, instructions):
         self.instructions = instructions
         self.position = 0
         self.halted = False
-        self.FUNCTIONS = {}
-
+        self.functions = {
+            1: self.sumOp,
+            2: self.multiplyOp,
+            3: self.saveOp,
+            4: self.printOp,
+            5: self.jumpIfTrueOp,
+            6: self.jumpIfFalseOp,
+            7: self.lessThanOp,
+            8: self.equalsOp,
+        }
 
     def get_parameter_value(self, parameter, mode):
-        if mode == MODES[POSITION]:
+        if mode == Mode.POSITION.value:
             return int(self.instructions[int(parameter)])
-        elif mode == MODES[IMMEDIATE]:
+        elif mode == Mode.IMMEDIATE.value:
             return int(parameter)
         raise Exception(f'Unknown mode {mode}')
 
-    def sumOp(self, modes, input):
+    def get_parameters(self, modes):
         if len(modes) >= 1:
             first_mode = modes[-1]
         else:
@@ -53,23 +49,18 @@ class Amplifier:
             second_mode = '0'
         first_parameter = self.get_parameter_value(self.instructions[self.position + 1], first_mode)
         second_parameter = self.get_parameter_value(self.instructions[self.position + 2], second_mode)
+        return (first_parameter, second_parameter)
+
+    def sumOp(self, modes, input):
+        parameters = self.get_parameters(modes)
         sum_index = self.instructions[self.position + 3]
-        self.instructions[sum_index] = first_parameter + second_parameter
+        self.instructions[sum_index] = parameters[0] + parameters[1]
         return (4, False)
 
     def multiplyOp(self, modes, input):
-        if len(modes) >= 1:
-            first_mode = modes[-1]
-        else:
-            first_mode = '0'
-        if len(modes) >= 2:
-            second_mode = modes[-2]
-        else:
-            second_mode = '0'
-        first_parameter = self.get_parameter_value(self.instructions[self.position + 1], first_mode)
-        second_parameter = self.get_parameter_value(self.instructions[self.position + 2], second_mode) #TODO: leading zero problem
+        parameters = self.get_parameters(modes)
         product_index = self.instructions[self.position + 3]
-        self.instructions[product_index] = first_parameter * second_parameter
+        self.instructions[product_index] = parameters[0] * parameters[1]
         return (4, False)
 
     def saveOp(self, modes, input):
@@ -91,19 +82,10 @@ class Amplifier:
         return self.jumpOp(modes, False, input)
 
     def jumpOp(self, modes, isIfTrue, input):
-        if len(modes) >= 1:
-            first_mode = modes[-1]
-        else:
-            first_mode = '0'
-        if len(modes) >= 2:
-            second_mode = modes[-2]
-        else:
-            second_mode = '0'
-        parameter = self.get_parameter_value(self.instructions[self.position + 1], first_mode)
-        second_parameter = self.get_parameter_value(self.instructions[self.position + 2], second_mode)
-        if((parameter != 0 and isIfTrue) or
-        (parameter == 0 and not isIfTrue)):
-            return (second_parameter - self.position, False)
+        parameters = self.get_parameters(modes)
+        if((parameters[0] != 0 and isIfTrue) or
+        (parameters[0] == 0 and not isIfTrue)):
+            return (parameters[1] - self.position, False)
         else:
             return (3, False)
 
@@ -114,35 +96,16 @@ class Amplifier:
         return self.compareOp(modes, lambda a, b: a == b, input)
 
     def compareOp(self, modes, comparator, input):
-        if len(modes) >= 1:
-            first_mode = modes[-1]
-        else:
-            first_mode = '0'
-        if len(modes) >= 2:
-            second_mode = modes[-2]
-        else:
-            second_mode = '0'
-        first_parameter = get_parameter_value(self.instructions[self.position + 1], first_mode, self.instructions)
-        second_parameter = get_parameter_value(self.instructions[self.position + 2], second_mode, self.instructions)
-        if(comparator(first_parameter, second_parameter)):
+        parameters = self.get_parameters(modes)
+        if(comparator(parameters[0], parameters[1])):
             self.instructions[self.instructions[self.position + 3]] = 1
         else:
             self.instructions[self.instructions[self.position + 3]] = 0
         return (4, False)
 
     def find_function(self, opcode):
-        functions = {
-            SUM: self.sumOp,
-            MULTIPLY: self.multiplyOp,
-            SAVE: self.saveOp,
-            PRINT: self.printOp,
-            JUMP_IF_TRUE: self.jumpIfTrueOp,
-            JUMP_IF_FALSE: self.jumpIfFalseOp,
-            LESS_THAN: self.lessThanOp,
-            EQUALS: self.equalsOp
-        }
-        if(opcode in functions):
-            return functions[opcode]
+        if(opcode in self.functions):
+            return self.functions[opcode]
 
     def execute_operation(self, opcode, modes, input):
         function = self.find_function(opcode)
@@ -151,17 +114,16 @@ class Amplifier:
     def run_int_code(self, input):
         if(self.halted == True):
             return input[0]
-        operations = self.instructions
         input_index = 0
-        while self.position < len(operations):
-            modes = str(operations[self.position])[:-2]
-            opcode = int(str(operations[self.position])[-2:])
+        while self.position < len(self.instructions):
+            modes = str(self.instructions[self.position])[:-2]
+            opcode = int(str(self.instructions[self.position])[-2:])
 
-            if operations[self.position] == PRINT:
+            if self.instructions[self.position] == Operation.PRINT:
                 value_to_print = self.execute_operation(opcode, modes, input[input_index])
                 self.position += 2
                 return value_to_print
-            elif operations[self.position] == HALT:
+            elif self.instructions[self.position] == Operation.HALT:
                 print("in halt")
                 self.halted = True
                 return input[0]
@@ -172,4 +134,4 @@ class Amplifier:
                     if(input_index < len(input) - 1):
                         input_index += 1
             
-        return operations[0]
+        return self.instructions[0]
