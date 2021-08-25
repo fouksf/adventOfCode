bda63bb9da7b2aabe867a6fb782710001794724b
Nick Ramsbottom <nick.ramsbottom@ft.com>
2021-08-25 14:32:43 +0100

    Running one too many times but works

--- 2019/7/day7.py
+++ 2019/7/day7.py
@@ -9,12 +9,14 @@ from amplifier import Amplifier
 
 phases = [9, 8, 7, 6, 5]
 # iterations = itertools.permutations(phases)
-example_opcodes = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
+# example_opcodes = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
 # example_input = [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,6,23,2,6,23,27,2,27,9,31,1,5,31,35,1,35,10,39,2,39,9,43,1,5,43,47,2,47,10,51,1,51,6,55,1,5,55,59,2,6,59,63,2,63,6,67,1,5,67,71,1,71,9,75,2,75,10,79,1,79,5,83,1,10,83,87,1,5,87,91,2,13,91,95,1,95,10,99,2,99,13,103,1,103,5,107,1,107,13,111,2,111,9,115,1,6,115,119,2,119,6,123,1,123,6,127,1,127,9,131,1,6,131,135,1,135,2,139,1,139,10,0,99,2,0,14,0]
-iterations = [[1,0,4,3,2]]
+iterations = [[9,7,8,5,6]]
 # print(run_int_code(example_input))
-
+# example_opcodes = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+example_opcodes = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
 outputs = []
+
 for iteration in iterations:
     output_signal = 0
     input_signal = 0
@@ -31,7 +33,7 @@ for iteration in iterations:
             output_signal = current_amplifier.run_int_code(input)
             print("amplifier " +  str(ampNumber) + " gave output: " + str(output_signal))
             input_signal = output_signal
-            if (output_signal == False):
+            if (current_amplifier.halted == True):
                 print(input)
                 print("what are we doing here?")
                 print("looks like amplifier" + ampNumber + " has halted")
