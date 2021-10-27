a103a25ea68f4528bc7c404796a172e3e9e2d472
Nick Ramsbottom <nick.ramsbottom@ft.com>
2021-10-27 14:36:24 +0100

    Sort points before using them

--- 2019/10/day10.py
+++ 2019/10/day10.py
@@ -33,14 +33,13 @@ def calc_distance(point_one, point_two):
 
     return math.sqrt((x - a)**2 + (y - b)**2)
 
-print(points)
-
 def calc_visible_astroids():
     a = points[0]
-    # for i, point in enumerate(points[1:]):
+    sorted_points = sorted(points, key=lambda point: calc_distance(point, a))
+    # for i, point in enumerate(sorted_points[1:]):
 
-    b = points[1]
-    c = points[2]
+    b = sorted_points[1]
+    c = sorted_points[2]
 
     a_b = calc_distance(a, b)
     a_c = calc_distance(a, c)
@@ -48,8 +47,10 @@ def calc_visible_astroids():
 
     if (a_b + b_c == a_c):
         if (a_b < a_c):
-            points.pop(1)
+            sorted_points.pop(1)
         else:
-            points.pop(2)
+            sorted_points.pop(2)
+
+    # return len(sorted_points)
         
         
