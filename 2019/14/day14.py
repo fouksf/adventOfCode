1e320793384d857d83ba98ccaf5de733c136a101
Nick Ramsbottom <nick.ramsbottom@ft.com>
2022-04-27 14:10:55 +0100

    Changes

--- 2019/14/day14.py
+++ 2019/14/day14.py
@@ -8,9 +8,13 @@ input = """10 ORE => 10 A
 7 A, 1 E => 1 FUEL"""
 
 instructions = input.split("\n")
+
 materials_needed = {
     "FUEL": 1
 }
+
+materials_stash = {}
+
 ore_count = 0
 
 def build_mapping():
@@ -26,18 +30,29 @@ reacts = build_mapping()
 while len(materials_needed) > 0:
     new_materials = {}
     for material in materials_needed:
-        print(materials_needed)
-        reaction = instructions[reacts[material]]
-        needed = reaction.split(" => ")[0].split(", ")
-        amount_needed = materials_needed[material]
+        print("needed:", materials_needed)
+        reaction = instructions[reacts[material]] # 7 A, 1 D => 1 E
+        needed = reaction.split(" => ")[0].split(", ") # ["7 A", "1 D"]
+        amount_needed = materials_needed[material] # integer eg 1 for FUEL at the start
+        amount_created = int(reaction.split(" => ")[1].split(" ")[0])
         for need in needed:
+            # amount_of_needed_in_stash = materials_stash[need]
             (amount, element) = need.split(" ")
+
+            excess = amount_created - amount_needed
+            if excess > 0:
+                if element in materials_stash:
+                    materials_stash[element] += excess
+                else:
+                    materials_stash[element] = excess
+
             if element == "ORE":
-                amount_created = int(reaction.split(" => ")[1].split(" ")[0])
-                first_thing = math.ceil(amount_needed / amount_created)
-                ore_count += first_thing * int(amount)
+                reaction_run_count = math.ceil(amount_needed / amount_created)
+                ore_count += reaction_run_count * int(amount)
                 continue
 
+            print("stash:", materials_stash)
+            
             if element in new_materials:
                 new_materials[element] += (amount_needed * int(amount))
             else:
