7a86ed6177b66087976650c0b40e9a2eedabb43f
Nick Ramsbottom <nick.ramsbottom@ft.com>
2022-05-18 13:47:14 +0100

    Add desired day 15 algo

--- 2019/15/day15.py
+++ 2019/15/day15.py
@@ -13,14 +13,43 @@ class RepairDroid:
         self.graph = {} # (3,0) -> ./D/#/O
         self.instructions = []
         self.current_position = Point(0, 0)
+        self.found = False
+        self.queue = [{
+            'point': Point(0, 0),
+            'steps': 0,
+        }]
+        self.visited = {}
+
+
+        self.directionMapping = {
+            1: self.get_northern_point,
+            2: self.get_southern_point,
+            3: self.get_eastern_point,
+            4: self.get_western_point,
+        }
 
     def reveal_map(self):
-        while True:
-            # check if already visited and try going N
+        while not self.Found:
+            current = self.queue.pop()
+
+            if current.point in self.visited:
+                continue
+
+            self.visited.add(current['point'])
+            steps = current.steps + 1
+
             for direction in range(1,5):
-            # if yes recursive
-            # else check if already visited and if not try E, S, W
-            # if cannot move to either direction go opposite last intrustion in instructions and continue from there
+                newPoint = self.directionMapping[direction](current.point)
+                newIntCode = self.amp.clone()
+
+                out = newIntCode.run(direction)
+
+                if out == 2:
+                    print(steps)
+                elif out == 0:
+                    self.visited.add(newPoint)
+                else:
+                    self.queue.push({'point': newPoint, 'steps': steps})
 
     def get_northern_point(self):
         return Point(self.current_position.x, self.current_position.y + 1)
