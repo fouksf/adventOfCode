35150e44ee976c2f678ea695a07e687086326368
Nick Ramsbottom <nick.ramsbottom@ft.com>
2022-05-18 13:58:24 +0100

    Make start on day 15, bug in output always being 0

--- 2019/15/day15.py
+++ 2019/15/day15.py
@@ -1,64 +1,63 @@
 from intcode import Computer
 from point import Point
+from copy import deepcopy
 
-## reveal the graph by dfs
-## breath first search to find the fastes way
-
-
-repairDroid = Computer(input)
+input = list(map(int, open("2019/11/input.txt", "r").read().split(",")))
 
 class RepairDroid:
     def __init__(self, amplifier):
         self.amp = amplifier
-        self.graph = {} # (3,0) -> ./D/#/O
-        self.instructions = []
-        self.current_position = Point(0, 0)
         self.found = False
         self.queue = [{
             'point': Point(0, 0),
             'steps': 0,
         }]
-        self.visited = {}
-
-
+        self.visited = set()
         self.directionMapping = {
             1: self.get_northern_point,
             2: self.get_southern_point,
-            3: self.get_eastern_point,
-            4: self.get_western_point,
+            4: self.get_eastern_point,
+            3: self.get_western_point,
         }
 
     def reveal_map(self):
-        while not self.Found:
+        while not self.found:
             current = self.queue.pop()
 
-            if current.point in self.visited:
+            if current['point'] in self.visited:
                 continue
 
             self.visited.add(current['point'])
-            steps = current.steps + 1
+            steps = current['steps'] + 1
 
             for direction in range(1,5):
-                newPoint = self.directionMapping[direction](current.point)
-                newIntCode = self.amp.clone()
-
-                out = newIntCode.run(direction)
-
+                newPoint = self.directionMapping[direction](current['point'])
+                newIntCode = deepcopy(self.amp)
+                out = newIntCode.run_int_code([direction])
+                print(out)
                 if out == 2:
+                    self.found = True
                     print(steps)
                 elif out == 0:
                     self.visited.add(newPoint)
                 else:
+                    print("in here")
                     self.queue.push({'point': newPoint, 'steps': steps})
+                
+                print(self.queue)
 
-    def get_northern_point(self):
-        return Point(self.current_position.x, self.current_position.y + 1)
+    def get_northern_point(self, point):
+        return Point(point.x, point.y + 1)
 
-    def get_southern_point(self):
-        return Point(self.current_position.x, self.current_position.y - 1)
+    def get_southern_point(self, point):
+        return Point(point.x, point.y - 1)
 
-    def get_eastern_point(self):
-        return Point(self.current_position.x + 1, self.current_position.y)
+    def get_eastern_point(self, point):
+        return Point(point.x + 1, point.y)
 
-    def get_western_point(self):
-        return Point(self.current_position.x - 1, self.current_position.y)
+    def get_western_point(self, point):
+        return Point(point.x - 1, point.y)
+
+intcode = Computer(input)
+droid = RepairDroid(intcode)
+droid.reveal_map()
