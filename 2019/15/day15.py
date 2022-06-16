27e3561f97c064c5d936e04eac054d72dcd957fd
Nick Ramsbottom <nick.ramsbottom@ft.com>
2022-06-16 13:38:43 +0100

    Day 15 part 2

--- 2019/15/day15.py
+++ 2019/15/day15.py
@@ -19,8 +19,9 @@ class RepairDroid:
             4: self.get_eastern_point,
             3: self.get_western_point,
         }
+        self.max_steps = 0
 
-    def reveal_map(self):
+    def go_to_o(self):
         while not self.found:
             current = self.queue.pop()
 
@@ -38,14 +39,39 @@ class RepairDroid:
                 if out == 2:
                     self.found = True
                     print(steps)
+                    return newIntCode
                 elif out == 0:
                     self.visited.add(newPoint)
                 else:
                     # print("in here")
                     self.queue.append({'point': newPoint, 'steps': steps, 'amp': newIntCode})
-                
                 # print(self.queue)
             
+    def time_oxygenation(self):
+        while len(self.queue) > 0:
+            current = self.queue.pop()
+
+            if current['point'] in self.visited:
+                continue
+
+            self.visited.add(current['point'])
+            steps = current['steps'] + 1
+
+            if steps > self.max_steps:
+                self.max_steps = steps
+
+            for direction in range(1,5):
+                newPoint = self.directionMapping[direction](current['point'])
+                newIntCode = deepcopy(current['amp'])
+                out = newIntCode.run_int_code([direction])
+                
+                if out == 0:
+                    self.visited.add(newPoint)
+                else:
+                    self.queue.append({'point': newPoint, 'steps': steps, 'amp': newIntCode})
+        
+        print(self.max_steps - 1)
+
 
     def get_northern_point(self, point):
         return Point(point.x, point.y + 1)
@@ -58,7 +84,11 @@ class RepairDroid:
 
     def get_western_point(self, point):
         return Point(point.x - 1, point.y)
+    
+    
 
 intcode = Computer(input)
 droid = RepairDroid(intcode)
-droid.reveal_map()
+amp_at_o = droid.go_to_o()
+
+time_droid = RepairDroid(amp_at_o).time_oxygenation()
