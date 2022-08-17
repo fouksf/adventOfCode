7531f4d7be8d8f0fc311482d855aa59907da2ee2
Nick Ramsbottom <nick.ramsbottom@ft.com>
2022-08-17 13:37:54 +0100

    translate js solution to python

--- 2019/16/day16.py
+++ 2019/16/day16.py
@@ -1,32 +1,29 @@
 import math
-import itertools
-input = list(map(int, list(open("2019/16/input.txt", "r").read())))
 
-def flatten_list(l):
-    return [x for xs in l for x in xs]
+inpt = list(map(int, list(open("2019/16/input.txt", "r").read().strip())))
 
-class FFT:
-    def __init__(self, input):
-        self.pattern = [0, 1, 0, -1]
-        self.input = input
+def desired_index(i, pos):
+    pattern = [0, 1, 0, -1]
+    index = math.floor((i + 1)/pos) % len(pattern)
+    return pattern[index]
 
-    def get_pattern_for_position(self, position):
-        single_pattern = flatten_list([[a] * position for a in self.pattern])
-        length_needed = len(self.input) + 1
-        times_to_repeat = math.ceil(length_needed / len(single_pattern))
-        return (single_pattern * times_to_repeat)[1:]
 
-    def calculate_digit(self, position, pattern):
-        itertools.reduce(pattern, lambda x => {
+def calculate(inp):
+    out = []
 
-        })
+    for pos in range(1, len(inp)+1):
+        sum = 0
 
-    def do_phase(self):
-        output = []
+        for i in range(0, len(inp)):
+            sum += desired_index(i, pos) * inp[i]
 
-        for digit, index in self.input:
-            pattern = get_pattern_for_position(index)
-            output.append(self.calculate_digit(position, pattern)
+        sum = abs(sum)
+        out.append(sum % 10)
 
-fft = FFT(input)
-print(fft.get_pattern_for_position(6))
+    return out
+
+
+for i in range(0, 100):
+    inpt = calculate(inpt)
+
+print(inpt)
