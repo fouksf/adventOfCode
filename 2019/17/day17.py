790e47ea3cb3c3fdd382e0825ea8d50697d3169c
Svetlana Tobova <svetlana.velichkova@ft.com>
2022-09-21 14:57:46 +0300

    WIP day17 pt.2

--- 2019/17/day17.py
+++ 2019/17/day17.py
@@ -19,10 +19,13 @@ class Scaffolding:
                 break
             else:
                 row.append(chr(num))
+                if(chr(num)) == '^':
+                    start_row = len(grid)
+                    start_col = len(row)
         self.grid = grid
 
     def print_grid(self):
-        for line in grid:
+        for line in self.grid:
             print(''.join(line))
 
     def isIntersection(self, i, j):
@@ -46,6 +49,55 @@ class Scaffolding:
                 sum += param
         return sum
 
+        # current orientation can be N, S, E, W
+        # return value can be L or R
+    def determine_direction_to_turn(self, x, y, old_x, old_y, current_orientation):
+        if x + 1 <= len(self.grid[0]) and self.grid[x + 1][y] == SCAFFOLDING:
+            if current_orientation == 'N':
+                return 'R'
+            elif current_orientation == 'S':
+                return 'L'
+            throw Exception("wtf")
+        if x - 1 <= len(self.grid[0]) and self.grid[x - 1][y] == SCAFFOLDING:
+            if current_orientation == 'N':
+                return 'L'
+            elif current_orientation == 'S':
+                return 'R'
+            throw Exception("wtf 2")
+        if y + 1 <= len(self.grid[0]) and self.grid[x][y + 1] == SCAFFOLDING:
+            if current_orientation == 'E':
+                return 'R'
+            elif current_orientation == 'W':
+                return 'L'
+            throw Exception("wtf 3")
+        if y - 1 <= len(self.grid[0]) and self.grid[x][y - 1] == SCAFFOLDING:
+            if current_orientation == 'W':
+                return 'R'
+            elif current_orientation == 'E':
+                return 'L'
+            throw Exception("wtf 4")
+
+    def is_scaffolding(self, x, y):
+        return self.grid[x][y] == SCAFFOLDING
+
+    def determine_lenght_forward(self, x, y, orientation):
+        delta = 1 if orientation == 'E' or orientation == 'N' else -1
+        if orientation == 'N' or orientation = 'S':
+            dy = y + delta
+            while self.is_scaffolding(x, dy):
+                dy += delta
+            return (dy - y) * delta
+
+        
+
+    def find_path_commands(self):
+        direction = determine_direction_to_turn()
+        # // determine how far we can go
+        # // repeat but exclude the direction we came from
+        # end when there is no other place to go except back
+
+
 scaffolding = Scaffolding(intcode, inp)
 print(scaffolding.calculate_sum_of_alignment_parameters())
+scaffolding.print_grid()
 
