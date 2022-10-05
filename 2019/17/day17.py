03b382865adf13d5d60c147cc97d463a692cfdd7
Svetlana Tobova <svetlana.velichkova@ft.com>
2022-10-05 15:57:05 +0300

    Break everything while trying to fix it

--- 2019/17/day17.py
+++ 2019/17/day17.py
@@ -53,25 +53,25 @@ class Scaffolding:
         # return value can be L or R
     def determine_direction_to_turn(self, position, current_orientation):
         (x, y) = position
-        if x + 1 <= len(self.grid[0]) and self.grid[x + 1][y] == self.SCAFFOLDING:
+        if x + 1 <= len(self.grid[0]) and self.grid[x + 1][y] == self.SCAFFOLDING and not self.came_from(position, current_orientation, (x + 1, y)):
             if current_orientation == 'E':
                 return ('R', 'S')
             elif current_orientation == 'W':
                 return ('L', 'S')
             raise Exception("wtf")
-        if x - 1 <= len(self.grid[0]) and self.grid[x - 1][y] == self.SCAFFOLDING:
+        if x - 1 <= len(self.grid[0]) and self.grid[x - 1][y] == self.SCAFFOLDING and not self.came_from(position, current_orientation, (x - 1, y)):
             if current_orientation == 'W':
                 return ('R', 'N')
             elif current_orientation == 'E':
                 return ('L', 'N')
             raise Exception("wtf 2")
-        if y + 1 <= len(self.grid[0]) and self.grid[x][y + 1] == self.SCAFFOLDING:
+        if y + 1 <= len(self.grid[0]) and self.grid[x][y + 1] == self.SCAFFOLDING and not self.came_from(position, current_orientation, (x, y + 1)):
             if current_orientation == 'N':
                 return ('R', 'E')
             elif current_orientation == 'S':
                 return ('L', 'E')
             raise Exception("wtf 3")
-        if y - 1 <= len(self.grid[0]) and self.grid[x][y - 1] == self.SCAFFOLDING:
+        if y - 1 <= len(self.grid[0]) and self.grid[x][y - 1] == self.SCAFFOLDING and not self.came_from(position, current_orientation, (x, y - 1)):
             if current_orientation == 'N':
                 return ('L', 'W')
             elif current_orientation == 'S':
@@ -79,12 +79,25 @@ class Scaffolding:
             raise Exception("wtf 4")
         raise Exception("wtf 5")
 
+    def came_from(self, current_position, orientation, point_to_check):
+        (x, y) = current_position
+        (x1, y1) = point_to_check
+        if orientation == 'E':
+            return x == x1 and y - 1 == y1
+        if orientation == 'W':
+            return x == x1 and y + 1 == y1
+        if orientation == 'S':
+            return x - 1 == x1 and y == y1
+        if orientation == 'N':
+            return x + 1 == x1 and y == y1
+        
+
     def is_scaffolding(self, x, y):
-        return self.grid[x][y] == self.SCAFFOLDING
+        return False if y <= len(self.grid[0]) and x <= len(self.grid) else self.grid[x][y] == self.SCAFFOLDING
 
     def determine_length_forward(self, position, orientation):
         (x, y) = position
-        delta = 1 if orientation == 'E' or orientation == 'N' else -1
+        delta = 1 if orientation == 'E' or orientation == 'S' else -1
         if orientation == 'E' or orientation == 'W':
             dy = y + delta
             while dy >= 0 and dy < len(self.grid[0]) and self.is_scaffolding(x, dy):
@@ -94,7 +107,7 @@ class Scaffolding:
             dx = x + delta
             while dx >= 0 and dx < len(self.grid) and self.is_scaffolding(dx, y):
                 dx += delta
-            return (dx - x) * delta
+            return (dx - x - delta) * delta
     
     def move(self, position, orientation, distance):
         (x, y) = position
@@ -117,7 +130,7 @@ class Scaffolding:
         position = (16, 12)
         orientation = 'N'
 
-        for x in range(0, 2):
+        for x in range(0, 10):
             turn, orientation = self.determine_direction_to_turn(position, orientation)
             directions.append(turn)
             forward = self.determine_length_forward(position, orientation)
@@ -131,6 +144,15 @@ class Scaffolding:
 
 
 scaffolding = Scaffolding(intcode, inp)
+scaffolding.print_grid()
+# print(scaffolding.came_from((0,1), "E", (0,0)))
+# print(scaffolding.came_from((0,0), "W", (0,1)))
+# print(scaffolding.came_from((0,0), "N", (1,0)))
+# print(scaffolding.came_from((1,0), "S", (0,0)))
+# print(scaffolding.came_from((0,1), "W", (0,0)))
+# print(scaffolding.came_from((0,0), "N", (0,1)))
+# print(scaffolding.came_from((0,0), "S", (1,0)))
+# print(scaffolding.came_from((1,0), "E", (0,0)))
 # print(scaffolding.calculate_sum_of_alignment_parameters())
 # scaffolding.print_grid()
 scaffolding.find_path_commands()
