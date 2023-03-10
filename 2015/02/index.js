86264965536e3679bad9e21ad7bbb066c6593e32
Nick Ramsbottom <nick.ramsbottom@lego.com>
2023-03-10 13:52:33 +0000

    2015 day 2 part 1 js

---
+++ 2015/02/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const input = fs.readFileSync('./input.txt').toString()
+
+function surfaceArea(w, h, l) {
+    return 2 * l * w + 2 * w * h + 2 * h * l
+}
+
+function findSlack(w, h, l) {
+    const biggest = Math.max(w, h, l)
+    return (w * h * l) / biggest
+}
+
+const lines = input.split('\n')
+
+let sqFt = 0
+
+for (const line of lines) {
+    const [l, w, h] = line.split('x').map(dim => Number(dim))
+    sqFt += surfaceArea(l, w, h) + findSlack(l, w, h)
+}
+
+console.log(sqFt)
