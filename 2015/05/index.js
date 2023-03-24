2a8ca032250cf42b215e65832fc9aea69530d01f
Nick Ramsbottom <nick.ramsbottom@lego.com>
2023-03-24 15:02:57 +0000

    2015 day 05 part 2 js

--- 2015/05/index.js
+++ 2015/05/index.js
@@ -1,41 +1,12 @@
 const fs = require('fs')
 const strings = fs.readFileSync(__dirname + '/input.txt').toString().split('\n')
-// const strings = ['ugknbfddgicrmopn', 'aaa', 'jchzalrnumimnmhp', 'haegwjzuvuyypxyu', 'dvszwmarrgswjxmb']
-function hasAtLeastThreeVowels(str) {
-    const replaced = str.replace(/[^aeiou]/ig,'')
-    return replaced.length >= 3
+// const strings = ['qjhvhtzxzqqjkmpb', 'xxyxx', 'uurcxstgmygtbstg', 'ieodomkazucvgmuy']
+function nice(str){
+    const vowels = str.match(/(\w).\1/g);
+    const doubles = str.match(/(\w\w).*\1/g);
+    return (vowels !== undefined && vowels !== null && vowels.length > 0)
+        && (doubles !== undefined && doubles !== null && doubles.length > 0)
 }
 
-function hasRepeatedChar(str) {
-    for(let i = 0; i < str.length - 1; i++) {
-        const a = str[i]
-        const b = str[i + 1]
-
-        if (a === b) {
-            return true
-        }
-    }
-
-    return false
-}
-
-function hasBadPairs(str) {
-    const badPairs = new Set(['ab', 'cd', 'pq', 'xy'])
-
-    for(let i = 0; i < str.length - 1; i++) {
-        const a = str[i]
-        const b = str[i + 1]
-        const pair = a + b
-        if (badPairs.has(pair)) {
-            return true
-        }
-    }
-
-    return false
-}
-
-function isNice(string) {
-    return hasAtLeastThreeVowels(string) && hasRepeatedChar(string) && !hasBadPairs(string)
-}
-
-console.log(strings.filter(isNice).length)
+// console.log(strings.filter(nice))
+console.log(strings.filter(nice).length)
